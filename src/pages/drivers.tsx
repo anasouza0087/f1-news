4040f530784c6afd3513eb1ea87bcd2b0a8fed5b
Ana <ana.souza0087@gmail.com>
2022-11-12 22:26:48 -0300

    feat: create f1-news next.js project

---
+++ src/pages/drivers.tsx
@@ -0,0 +1,8 @@
+
+export default function Drivers() {
+    return (
+        <div>
+            Drivers
+        </div>
+    )
+}
---
+++ src/pages/grandPrix.tsx
@@ -0,0 +1,8 @@
+
+export default function GrandPrix() {
+    return (
+        <div>
+            GrandPrix
+        </div>
+    )
+}
